ad1133a18cf68c9cd45e92a72dedd1c2286b5db0
Ben Mordue <bmordue@adobe.com>
2016-03-15 13:50:25 +0000

    Basic logging

---
+++ test/log.test.js
@@ -0,0 +1,10 @@
+var log = require("../lib/log");
+
+describe("logger", function() {
+	var levels = ["error", "warn", "info", "debug"];
+	levels.forEach(function(level) {
+		it("should log at level " + level, function() {
+			log[level]({details: 'none'}, "Logging at level " + level);
+		});
+	});
+});
