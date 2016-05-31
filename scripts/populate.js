3787be61dacbb918eeae78ec191ff24fb071c65b
Ben Mordue <bmordue@adobe.com>
2016-05-31 17:10:44 +0100

    Rate limiting

--- scripts/populate.js
+++ scripts/populate.js
@@ -5,11 +5,62 @@ var DbConnector = require("../lib/db_connector");
 var fs = require("fs");
 var log = require("../lib/log");
 var path = require("path");
+var priceAsin = require("../lib/price-for-asin");
+var RateLimiter = require("limiter").RateLimiter;
 var util = require("util");
 
 var dbCon = new DbConnector();
+var limiter = new RateLimiter(1, "second");
 
 var nodes_count = 0;
+
+function throttledPriceLookup (asin, callback) {
+	limiter.removeTokens(1, function(err, remaining) {
+		priceAsin.fetch(asin, callback);
+	});
+}
+
+function processItem(parentAsin, item, callback) {
+	nodes_count++;
+	throttledPriceLookup(item.ASIN, function(err, result) {
+		if (err) {
+			log.error(err, "error looking up price for ASIN " + item.ASIN);
+		} else {
+			item.price = result.price;
+			item.currency = result.currency;
+		}
+		dbCon.createChildBookNode(parentAsin, item, function(err, result) {
+			if (err) {
+				log.error(item, "error adding node: ");
+				return callback(err);
+			}
+			callback();
+		});
+	});
+}
+
+function processFile(filename, callback) {
+	if (filename.length != 15 || filename.slice(-5) != ".json") {
+		return callback(); // primitive filter for interesting files
+	}
+	var parentAsin = filename.slice(0,-5); // asin.JSON -> asin
+	fs.readFile(path.join("output", filename), function(err, data) {
+		if (err) {
+			return callback(err);
+		}
+		log.info(filename, "read file ");
+		var item_list = [];
+		try {
+			item_list = JSON.parse(data).Items.Item;
+		} catch (e) {
+			return callback(e);
+		}
+		//log.debug(item_list, "item_list: ")
+		async.each(item_list, function(item, each_cb) {
+			processItem(parentAsin, item, each_cb);
+		}, callback);
+	});
+}
 
 function populate(callback) {
 	// expect dir output to be filled with files like "B00TOOSCC6.json"
@@ -18,35 +69,7 @@ function populate(callback) {
 			return callback(err);
 		}
 		log.info(files.length, "files found: ");
-		async.each(files, function(filename, file_cb) {
-			if (filename.length != 15 || filename.slice(-5) != ".json") {
-				return file_cb(); // primitive filter for interesting files
-			}
-			var parentAsin = filename.slice(0,-5); // asin.JSON -> asin
-			fs.readFile(path.join("output", filename), function(err, data) {
-				if (err) {
-					return file_cb(err);
-				}
-				log.info(filename, "read file ");
-				var item_list = [];
-				try {
-					item_list = JSON.parse(data).Items.Item;
-				} catch (e) {
-					return file_cb(e);
-				}
-				//log.debug(item_list, "item_list: ")
-				async.each(item_list, function(item, item_cb) {
-					nodes_count++;
-					dbCon.createChildBookNode(parentAsin, item, function(err, result) {
-						if (err) {
-							log.error(item, "error adding node: ");
-							return item_cb(err);
-						}
-						item_cb();
-					});
-				}, file_cb);
-			});
-		}, callback);
+		async.each(files, processFile, callback);
 	});
 }
 
