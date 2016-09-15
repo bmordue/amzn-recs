adf1b6fa068a4d81558c10526a8d6896d8a96457
Ben Mordue <benmordue@gmail.com>
2016-09-15 20:22:30 +0100

    message_queue.shift()

--- test/lib_tests/message_queue.test.js
+++ test/lib_tests/message_queue.test.js
@@ -1,7 +1,23 @@
 var assert = require("assert");
+var async = require("async");
 var fs = require("fs");
 var MessageQueue = require("../../lib/message_queue");
 var util = require("util");
+
+// TODO: make test assertions beyond lack of error
+
+function dumpDb(db, callback) {
+	var queryStr = util.format("SELECT *, rowid from %s", CRAWL_TASKS_TABLE_NAME);
+	var stmt = db.prepare(queryStr);
+	stmt.all(function(err, rows) {
+		if (err) {
+			return callback(err);
+		}
+		stmt.finalize(function(err) {
+			callback(err, rows);
+		});
+	});
+}
 
 describe("message queue", function() {
 	var testDbPath = "./temp/testDb.sqlite";
@@ -15,11 +31,19 @@ describe("message queue", function() {
 			queue.init(done);
 		});
 	});
-	
+
+	before(function() {
+		if (process.env.SQLITE_TRACE) {
+			queue.db.on('trace', function(query) {
+				console.log('SQLITE TRACE: ' + query);
+			});
+		}
+	});
+
 	it("should add to queue", function(done) {
 		var job = {
 			asin: 1,
-			token: 'test-test-test'
+			token: 'test-one'
 		};
 		queue.add(job, function(err, job_id) {
 			if (err) {
@@ -34,13 +58,28 @@ describe("message queue", function() {
 		});
 	});
 
-	it("should shift first item from queue", function(done) {
-		queue.shift(function(err, result) {
-			console.log(util.format("Result is %j", result));
-			done(err);
-		});
+	it("should add several items to queue", function(done) {
+		async.times(5, function(n, cb) {
+			var job = {
+				token: "test-two",
+				asin: "test-asin-" + n
+			};
+			queue.add(job, cb);
+		}, done);
 	});
-	
+
+	it("should shift first item from queue", function(done) {
+		queue.shift(done);
+	});
+
+	it("should shift several items from queue", function(done) {
+		async.timesSeries(3, function(n, cb) {
+			queue.shift(function(err, result) {
+				cb(err);
+			});
+		}, done);
+	});
+
 	after(function(done) {
 		queue.close(done);
 	});
