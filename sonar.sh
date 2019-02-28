5718e39b53e3a3568421c29edbdd040e06ea374e
Ben Mordue <benmordue@gmail.com>
2019-02-28 11:12:51 -0500

    Coverage and static analysis

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,5 @@
-docker run --rm -v $(pwd):/proj -w /proj \
+source .env
+sudo docker run --rm -v $(pwd):/proj -w /proj \
   -v $(pwd)/sonar.properties:/root/sonar-scanner/conf/sonar-scanner.properties \
   newtmitch/sonar-scanner:3.2.0-alpine \
   sonar-scanner \
