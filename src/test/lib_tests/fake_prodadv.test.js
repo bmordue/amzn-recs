d4edbab6628476b61c54e370efecc2af42d14b6f
Ben Mordue <benmordue@gmail.com>
2019-03-07 06:42:45 -0500

    more codefactor feedback

--- src/test/lib_tests/fake_prodadv.test.js
+++ src/test/lib_tests/fake_prodadv.test.js
@@ -1,13 +1,5 @@
 require("dotenv").load({silent: true});
-var DbConnector = require("../../lib/graphdb_connector");
-var fs = require("fs");
-var path = require("path");
 var should = require("should");
-var test_utils = require("../test_utils");
-var util = require("util");
-
-var LOG_ALL = false;
-
 var fake_prodadv = require('../../lib/fake_prodadv');
 
 describe("fake_prodadv", function() {
