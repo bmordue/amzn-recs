3e1e9372ff25759cd6f2e25b24baadd0dc2bafc8
Ben Mordue <BMordue@hymans.co.uk>
2020-11-02 16:45:34 +0000

    avoid session().run()

--- src/lib/graphdb_connector.ts
+++ src/lib/graphdb_connector.ts
@@ -3,6 +3,7 @@ import config = require("./config");
 import log = require("./log");
 import neo4j = require("neo4j-driver");
 import StatsD = require("node-statsd");
+import { toNumber } from "neo4j-driver/types/integer";
 
 const statsd = new StatsD({
 	prefix: 'amzn-recs.graphdb_connector.',
@@ -10,7 +11,6 @@ const statsd = new StatsD({
 });
 
 function closeAndCallback(callback, session, err = null, result = null) {
-	log.debug(null, "closeAndCallback");
 	if (err) {
 		log.error(null, err);
 		statsd.increment('query_error');
@@ -23,27 +23,23 @@ function closeAndCallback(callback, session, err = null, result = null) {
 	session.close().then(() => callback(err, result));
 }
 
-function createChildBookNode(driver, data, callback) {
+async function createChildBookNode(driver: neo4j.Driver, data, callback) {
 	const query = buildMergeWithPriceQuery(data);
-	const session = driver.session();
 
 	if (!query.params) {
 		log.warn(query, 'Empty params object');
 	}
 
-	const text = query.text;
-	session.run(text, query.params)
-		.subscribe({
-			onNext: () => { },
-			onCompleted: function () {
-				return closeAndCallback(callback, session);
-			},
-			onError: (err) => {
-				log.debug(query, 'failed query');
-				log.debug({}, err);
-				return closeAndCallback(callback, session, err);
-			}
-		});
+	let err = null;
+	const session = driver.session();
+	try {
+		await session.readTransaction((tx) => tx.run(query.text, query.params));
+	} catch(e) {
+		err = e;
+	} finally {
+		await session.close();
+		callback(err);
+	}
 }
 
 function buildMergeWithPriceQuery(data) {
@@ -81,7 +77,7 @@ function buildMergeWithPriceQuery(data) {
 	return { text: mergeQueryStr, params: mergeParams };
 }
 
-function addParentChildRelation(driver, parentAsin, childAsin, callback) {
+async function addParentChildRelation(driver: neo4j.Driver, parentAsin, childAsin, callback) {
 	const queryStr = "MATCH (parent:Book {ASIN: $parentAsin}),(child:Book {ASIN: $childAsin}) MERGE (parent)-[r:SIMILAR_TO]->(child) RETURN r";
 	const params = {
 		parentAsin: parentAsin,
@@ -89,18 +85,19 @@ function addParentChildRelation(driver, parentAsin, childAsin, callback) {
 	};
 
 	const session = driver.session();
-
-	session.run(queryStr, params)
-		.subscribe({
-			onNext: () => { },
-			onCompleted: function (summary) {
-				closeAndCallback(callback, session, null, summary);
-			},
-			onError: function (err) { closeAndCallback(callback, session, err); }
-		});
+	let err = null;
+	let result = null;
+	try {
+		result = await session.writeTransaction((tx) => tx.run(queryStr, params));
+	} catch (error) {
+		err = error
+	} finally {
+		await session.close();
+		callback(err, result);
+	}
 }
 
-function addAuthorRelations(driver, data, callback) {
+function addAuthorRelations(driver: neo4j.Driver, data, callback) {
 	if (!data.ItemAttributes || !data.ItemAttributes.Author) {
 		return callback(null, {});
 	}
@@ -108,45 +105,45 @@ function addAuthorRelations(driver, data, callback) {
 	if (authorList.constructor !== Array) {
 		authorList = [authorList];
 	}
-	const session = driver.session();
-	async.each(authorList, function (author, each_cb) {
-		const queryStr =
-			"MATCH (b:Book {ASIN: $childAsin})" +
-			" MERGE (a:Author {name: $author})" +
-			" MERGE (b)<-[:AUTHOR_OF]-(a)";
+	const queryStr =
+		"MATCH (b:Book {ASIN: $childAsin})" +
+		" MERGE (a:Author {name: $author})" +
+		" MERGE (b)<-[:AUTHOR_OF]-(a)";
+	async.each(authorList, async function (author, each_cb) {
 		const params = {
 			childAsin: data.ASIN,
 			author: author
 		};
-		session.run(queryStr, params)
-			.subscribe({
-				onNext: () => { },
-				onCompleted: function () { each_cb(); },
-				onError: each_cb
-			}
-			);
-	}, function (err, result) {
-		closeAndCallback(callback, session, err, result);
-	});
+		let err = null;
+		const session = driver.session();
+		try {
+			await session.writeTransaction((tx) => { tx.run(queryStr, params) });
+		} catch (e) {
+			err = e;
+		} finally {
+			await session.close();
+			each_cb(err);
+		}
+	}, callback);
 }
 
-function simpleQuery(connector, query, callback) {
+async function simpleQuery(connector: DbConnector, query: {text: string, parameters?: object}, callback) {
 	const session = connector.driver.session();
 
-	const records = [];
-	session.run(query.text, query.parameters)
-		.subscribe({
-			onNext: function (record) {
-				records.push(record);
-			},
-			onCompleted: function () {
-				closeAndCallback(callback, session, null, records);
-			},
-			onError: function (err) { closeAndCallback(callback, session, err); }
-		});
+	let records = null;
+	let err = null;
+	try {
+		const result = await session.writeTransaction((tx) => tx.run(query.text, query.parameters));
+		records = result.records;
+	} catch (e) {
+		err = e;
+	} finally {
+		await session.close();
+		callback(err, records);
+	}
 }
 
-function simpleQueryForAsin(connector, text, asin, callback) {
+function simpleQueryForAsin(connector, text: string, asin: string, callback: Function) {
 	const query = {
 		text: text,
 		parameters: { ASIN: asin }
@@ -176,6 +173,7 @@ export class DbConnector {
 	}
 
 	run(query: string, parameters, callback: Function) {
+		log.warn({}, "DbConnector.run() should only be used for testing and ad-hoc scripts");
 		const session = this.driver.session();
 		let records = [];
 		session.run(query, parameters).subscribe({
@@ -186,21 +184,20 @@ export class DbConnector {
 	}
 
 
-	init(callback) {
+	async init(callback) {
 		const session = this.driver.session();
-
-		session.run('CREATE CONSTRAINT ON (book:Book) ASSERT book.ASIN IS UNIQUE')
-			.subscribe({
-				onNext: () => { },
-				onCompleted: function () {
-					session.run('CREATE CONSTRAINT ON (author:Author) ASSERT author.name IS UNIQUE')
-						.subscribe({
-							onCompleted: function (summary) { closeAndCallback(callback, session, null, summary); },
-							onError: function (err) { closeAndCallback(callback, session, err); }
-						});
-				},
-				onError: function (err) { closeAndCallback(callback, session, err); }
-			});
+		const uniqueAsin = 'CREATE CONSTRAINT ON (book:Book) ASSERT book.ASIN IS UNIQUE';
+		const uniqueAuthor = 'CREATE CONSTRAINT ON (author:Author) ASSERT author.name IS UNIQUE';
+		let err = null;
+		try {
+			await session.writeTransaction((tx) => tx.run(uniqueAsin));
+			await session.writeTransaction((tx) => tx.run(uniqueAuthor));
+		} catch (error) {
+			err = error;
+		} finally {
+			session.close();
+			callback(err);
+		}
 	}
 
 
@@ -208,7 +205,6 @@ export class DbConnector {
 
 
 
-	// TODO: what is the purpose of newNodeResult?
 	createChildBookNodeAndRelations = function (parentAsin, data, callback) {
 		const self = this;
 		const newNodeResult = [];
@@ -222,29 +218,27 @@ export class DbConnector {
 			function (result, cb) {
 				addAuthorRelations(self.driver, data, cb);
 			}
-		],
-			function (err) {
-				return callback(err, newNodeResult);
-			});
+		], callback);
 	};
 
 	// TODO: createBookNode and createChildBookNode seem too similar; remove one
-	createBookNode = function (data, callback) {
-		log.debug(null, "DbConnector.createBookNode");
+	async createBookNode (data, callback) {
 		if (data.ItemAttributes.ProductGroup != "eBooks") {
 			log.warn(data, "Expected ItemAttributes.ProductGroup to be eBooks");
 		}
 		const query = buildMergeWithPriceQuery(data);
-		const session = this.driver.session();
-		log.debug(query, "Query: ");
-		const records = [];
-
-		session.run(query.text, query.params)
-			.subscribe({
-				onNext: (nextRecord) => { log.debug(nextRecord, "onNext"); records.push(nextRecord); },
-				onCompleted: function () { log.debug(null, "onCompleted"); closeAndCallback(callback, session, null, records[0]?.get('b')); },
-				onError: function (err) { log.debug({ err: err, query: query }, "onError: "); closeAndCallback(callback, session, err); }
-			});
+		const session: neo4j.Session = this.driver.session();
+		log.debug(query, "DbConnector.createBookNode query: ");
+		let createdNode;
+		let err;
+		try {
+			const result = await session.writeTransaction((tx) => tx.run(query.text, query.params));
+			createdNode = result.records[0]?.get('b');
+		} catch (error) {
+			err = error;
+		} finally {
+			callback(err, createdNode);
+		}
 	};
 
 
@@ -258,36 +252,22 @@ export class DbConnector {
 	};
 
 	deleteBookNode = function (asin, callback) {
-		const text = "MATCH (n { ASIN: $ASIN }) DETACH DELETE n RETURN COUNT(n)";
-		simpleQueryForAsin(this, text, asin, function(err, res) {
+		const queryStr = "MATCH (n { ASIN: $ASIN }) DETACH DELETE n RETURN COUNT(n)";
+		simpleQueryForAsin(this, queryStr, asin, function(err, res) {
 			const key = "COUNT(n)";
 			callback(err, res[0]?.get(key));
 		});
 	};
 
 	countOutgoingRecommendations = function (asin, callback) {
-		const query = {
-			text: "MATCH (n { ASIN: $ASIN })-[r]->() RETURN COUNT(DISTINCT r) AS outgoing",
-			params: { ASIN: asin }
-		};
+		const queryStr = "MATCH (n { ASIN: $ASIN })-[r]->() RETURN COUNT(DISTINCT r) AS outgoing";
 		const session = this.driver.session();
-
-		session.run(query)
-			.subscribe({
-				onNext: function (result) {
-					log.debug({ result: result }, 'outgoing relationships');
-				},
-				onCompleted: function (summary) {
-					closeAndCallback(callback, session, null, summary);
-				},
-				onError: function (err) { closeAndCallback(callback, session, err); }
-			});
+		simpleQueryForAsin(this, queryStr, asin, callback);
 	};
 
 	listAllAsins = function (callback) {
 		const query = {
-			text: "MATCH (n:Book) RETURN n.ASIN AS asin",
-			params: {}
+			text: "MATCH (n:Book) RETURN n.ASIN AS asin"
 		};
 		simpleQuery(this, query, function (err, summary) {
 			callback(err, summary);
@@ -296,8 +276,7 @@ export class DbConnector {
 
 	listLeafNodeAsins = function (callback) {
 		const query = {
-			text: "MATCH (n) WHERE NOT (n)-->() RETURN n.ASIN as asin;",
-			params: {}
+			text: "MATCH (n) WHERE NOT (n)-->() RETURN n.ASIN as asin;"
 		};
 		simpleQuery(this, query, function (err, summary) {
 			callback(err, summary);
