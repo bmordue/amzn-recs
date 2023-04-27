e0812afd73bb615143f001a494b7db2698f5b80f
Ben Mordue <BMordue@hymans.co.uk>
2023-04-27 10:23:22 +0100

    resolve tslint errors

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,4 +1,4 @@
-let http = require('http');
+import * as http from 'http';
 
 module.exports = {
   init,
@@ -6,22 +6,22 @@ module.exports = {
 }
 
 function init() {
-  return function () {
+  return () => {
     return {
       call(options, callback) {
-        let version = options.version || 'latest';
-        let endpoint = options.endpoint || '';
-        let url = 'http://169.254.169.254/' + version + '/meta-data/' + endpoint;
+        const version = options.version || 'latest';
+        const endpoint = options.endpoint || '';
+        const url = 'http://169.254.169.254/' + version + '/meta-data/' + endpoint;
 
-        http.get(url, function(res) {
-          let string = '';
+        http.get(url, (res) => {
+          let data = '';
           res.setEncoding('utf8');
-          res.on('data', function(chunk) {
-            string += chunk;
+          res.on('data', (chunk) => {
+            data += chunk;
           });
-          res.on('end', function() {
-            if (res.statusCode == 200) {
-              return callback(null, string);
+          res.on('end', () => {
+            if (res.statusCode === 200) {
+              return callback(null, data);
             } else {
               return callback(new Error('HTTP ' + res.statusCode +
                 ' when fetching credentials from EC2 API'));
@@ -37,25 +37,25 @@ function init() {
 
 // Try to get access id and secret key from ec2 metadata API
 function readCredentials(obj, cb) {
-  let lapse = obj.expires == null ? 0 : +new Date() - Date.parse(obj.expires);
+  const lapse = obj.expires == null ? 0 : +new Date() - Date.parse(obj.expires);
   if (obj.secretAccessKey == null || obj.accessKeyId == null || lapse > 0) {
-    let md = init();
-    md().call({endpoint: 'iam/security-credentials/'}, function(err, res) {
+    const md = init();
+    md().call({endpoint: 'iam/security-credentials/'}, (err, res) => {
       if (err) return cb(err);
       if (typeof res === 'undefined') return cb(new Error('metadata API response undefined'));
       md().call({endpoint: 'iam/security-credentials/' + res.split('\n')[0]},
-       function(err, res) {
+       (_err, innerRes) => {
         try {
-          res = JSON.parse(res);
+          innerRes = JSON.parse(innerRes);
         } catch(e) {
           return cb(e);
         }
-        if (res.SecretAccessKey === null)
+        if (innerRes.SecretAccessKey === null)
           return cb(new Error("secretAccessKey and accessKeyId not provided and could not be determined."));
-        obj.secretAccessKey = res.SecretAccessKey;
-        obj.accessKeyId = res.AccessKeyId;
-        obj.token = res.Token;
-        obj.expires = res.Expiration;
+        obj.secretAccessKey = innerRes.SecretAccessKey;
+        obj.accessKeyId = innerRes.AccessKeyId;
+        obj.token = innerRes.Token;
+        obj.expires = innerRes.Expiration;
         cb(null, obj);
       });
     });
