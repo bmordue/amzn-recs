1babec20ae91ca3974d5999e8c4e00037b357090
Ben Mordue <BMordue@hymans.co.uk>
2023-04-27 11:34:56 +0100

    formatting etc

--- src/test/lib_tests/aws.test.ts
+++ src/test/lib_tests/aws.test.ts
@@ -1,11 +1,17 @@
-import * as should from 'should';
-import {createProdAdvClient} from '../../lib/aws';
+import * as should from "should";
+import { createProdAdvClient, hmacSha256 } from "../../lib/aws";
 
-describe('fake_aws', function () {
+describe("fake_aws", function () {
   this.timeout(10000);
 
-    it('createProdAdvClient', () => {
-        const client = createProdAdvClient();
-        should.exist(client);
-    });
+  it("createProdAdvClient", () => {
+    const client = createProdAdvClient();
+    should.exist(client);
+  });
+
+  it("hmacSha256", () => {
+    hmacSha256("sooperseekrit", "my confidential message").should.eql(
+      "tZBLhzNb9yK5/+30iPYd1WAiL8PxaPjqsPLGffCDCvo="
+    );
+  });
 });
